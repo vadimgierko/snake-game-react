d842bb27e0ad3e10c0857c24ce4bbf9a714457b9
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2022-05-04 15:07:59 +0200

    readme file, header & footer updated

--- src/App.js
+++ src/App.js
@@ -87,7 +87,6 @@ export default function App() {
 		<div className="App">
 			<header>
 				<h1>Snake Game</h1>
-				<p>the app is in developing process... Don't use it for a moment ;-)</p>
 				<hr />
 			</header>
 			<main>
@@ -97,6 +96,39 @@ export default function App() {
 					{start ? "pause" : "start"}
 				</button>
 			</main>
+			<br />
+			<hr />
+			<footer>
+				<p>
+					created by{" "}
+					<a
+						href="https://github.com/vadimgierko"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Vadim Gierko
+					</a>{" "}
+					| 2022
+				</p>
+				<p>
+					<a
+						href="https://github.com/vadimgierko/snake-game-react"
+						target="_blank"
+						rel="noreferrer"
+					>
+						source code on GitHub
+					</a>{" "}
+					|{" "}
+					<a href="" target="_blank" rel="noreferrer">
+						CodeSandbox demo
+					</a>
+				</p>
+				<p>
+					The game was written in React entirely by me for entertainment and
+					learning purposes. I have not used any tutorial to be able to test
+					myself in writing logic & handle timer & event listeners.
+				</p>
+			</footer>
 		</div>
 	);
 }
